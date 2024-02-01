06594a71de7dd48ab87db1b6046398088e98e398
hiccup2735 <yujinhirose2735@gmail.com>
2024-02-01 17:29:26 +0100

    FrontEnd Update

---
+++ public_link/public/familjen-grotesk/README.md
@@ -0,0 +1,8 @@
+# Familjen Grotesk
+
+Familjen Grotesk is a typeface family suitable for both text and headlines.
+
+The Latin component was designed by Anders Wikström, Jonas Bäckman, Matilda Gysing, Kristian Möller (Familjen STHLM AB (https://familjen.se)). 
+
+Familjen Grotesk project is led by Kristian Möller, a type designer based in Stockholm. 
+To contribute, see [github.com/Familjen-Sthlm/Familjen-Grotesk](https://github.com/Familjen-Sthlm/Familjen-Grotesk)
---
+++ public_link/src/pages/chat/Chat/SentChat.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function SentChat({user, avatar, text}) {
+  return (
+    <div className="flex justify-end">
+      <div className="flex w-11/12 flex-row-reverse">
+        <div className='flex-col items-center justify-center flex'>
+          <img
+            src={avatar}
+            alt="avatar"
+            className="inline-block h-10 w-10 rounded-full"
+          />  
+          <div>{user}</div>
+        </div>
+
+        <div className="mr-4" />
+        <div className="relative max-w-xl rounded-xl rounded-tr-none bg-blue-500 px-4 py-2 self-start">
+          <span className="text-sm font-medium text-white">
+          {text}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
